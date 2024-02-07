c7a1065de1e500e18c83ab50c3b1cbfae2a39c62
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-02-07 10:48:14 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+def guess():
+    r = random.randint(0,10)
+    total_chances = 10
+
+    while True:
+        i = int(input("Guess the number between 0 and 10: "))
+
+
+        if i == r:
+            print("You guessed! Nice!")
+            break
+
+        if i >= r:
+            print("Try lower!")
+            total_chances -= 1
+        elif i <= r:
+            print("Try higher!")
+            total_chances -= 1
+
+        if total_chances == 0:
+            print("You have 0 chances! You lost!")
+            break
+
+        print(f"You have {total_chances} left!")
+
+
+
+guess()
